6a27c0ee9896c13da5e6f1b5ee6d51452c6fcde8
magur0don <magurodon.10000yen@gmail.com>
2020-09-07 21:33:10 +0900

    firstCommit

---
+++ 2DPuzzleGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallController : MonoBehaviour, IPointerClickHandler
+{
+    private SpriteRenderer m_spriteRenderer = null;
+    private Rigidbody2D m_rigidbody2D = null;
+    Vector2 m_currentVelocity = Vector2.zero;
+
+    bool m_MoveLimit = false;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        m_spriteRenderer.gameObject.SetActive(false);
+    }
+}
